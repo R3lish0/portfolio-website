04073cb7dde97b72fb7e04ebd92f3c48b5b74e54
Eli Newlin <elinewlin@gmail.com>
2024-08-08 08:56:29 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio-website
+
+Here is a website in which I can display some of my experiences and projects
--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.getElementById("experience").addEventListener("click", function () {
   });
 });
 
+document.getElementById("projects").addEventListener("click", function () {
+  document.getElementById("page-three-container").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("passion").addEventListener("click", function () {
+  document.getElementById("page-four-container").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
 document.getElementById("experience-2").addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
@@ -33,5 +45,101 @@ document
     document.body.style.overflow = "auto";
 
     var myDiv = document.getElementById("mobile-blurb-1");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("project-1").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-3");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-3-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-3");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("project-2").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-4");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-4-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-4");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("project-3").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-5");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-5-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-5");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("passion-1").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-6");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-6-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-6");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("passion-2").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-7");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-7-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-7");
+    myDiv.classList.remove("show");
+  });
+
+document.getElementById("passion-3").addEventListener("click", function () {
+  document.body.style.overflow = "hidden";
+
+  var myDiv = document.getElementById("mobile-blurb-8");
+  myDiv.classList.add("show");
+});
+
+document
+  .getElementById("mobile-blurb-8-close")
+  .addEventListener("click", function () {
+    document.body.style.overflow = "auto";
+
+    var myDiv = document.getElementById("mobile-blurb-8");
     myDiv.classList.remove("show");
   });
